6c5821b70b8d4374245e41a2c611904b95eeda17
anima-regem <vichukartha@gmail.com>
2023-04-11 17:15:13 +0530

    3

---
+++ script.js
@@ -0,0 +1,17 @@
+const darkBtn = document.getElementById('butnDrk');
+const lightBtn = document.getElementById('butnLgt');
+
+// swap style css file to styleDark
+function swapStyleSheet(sheet) {
+  document.getElementById('pagestyle').setAttribute('href', sheet);
+}
+
+// Dark Mode
+darkBtn.onclick = function() {
+    swapStyleSheet('styleDark.css');
+    }
+
+// Light Mode
+lightBtn.onclick = function() {
+    swapStyleSheet('style.css');
+    }
